7ea8b35829c5adfb868ab5ecba1bca4099ef1be7
Prasad Ramachandra Lakshmi <74173550+PrasadRamachandraLakshmi@users.noreply.github.com>
2021-09-24 07:33:55 +0200

    Update perception_node.sh

--- ros_code/02-01-object-detection-exercise/ROS/perception_pkg/perception_node.sh
+++ ros_code/02-01-object-detection-exercise/ROS/perception_pkg/perception_node.sh
@@ -5,6 +5,8 @@ dir=/home/freicar/freicar_ws/src/ros_code/02-01-object-detection-exercise/ROS/pe
 
 source /opt/conda/etc/profile.d/conda.sh
 conda activate freicar
+pip install visdom
+pip install webcolors
 
 cd $dir
 python perception_node.py -n $1 -w $2
